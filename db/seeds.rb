304f0cf64cda9d9e54682ec3191531b760155bb2
Roger Mendez <roger61087@gmail.com>
2022-02-17 18:57:33 -0500

    commit tree e2c731e8ca2a1cec88cf53a8bed4b49cd0880d5c

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create(title: "Cooking Ribeye", instructions: "Place the steaks on the grill.Prepare your seasonings.Cook the 
+    steak for 2 to 3 minutes on each side until it reaches your desi")
